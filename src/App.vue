dc31fb50ddfe6f2bb2bc3db7886ac55c7925eb76
DenisConsta <denisconstantin338@gmail.com>
2023-01-26 15:52:25 +0100

    Milestone #3 axios call

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script >
+import axios from "axios";
+
+  export default{
+    name: 'App',
+    data(){
+      return{
+        projects: [],
+        basicUrl: 'http://127.0.0.1:8000/api/',
+      }
+    },
+    methods:{
+      getApi(){
+
+        axios.get(this.basicUrl + 'projects', {
+            params:{              
+            }
+          })
+          .then((result) => {
+            this.projects = result.data.projects;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    mounted(){
+      this.getApi();
+    }
+  }
+
+</script>
+
+<template>
+  <h1 class="display-1">Hello Vue!</h1>  
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+
+</template>
+
+<style scoped lang="scss">
+@use './styles/general.scss' as *;
+
+h1{
+  color: red;
+}
+
+</style>
